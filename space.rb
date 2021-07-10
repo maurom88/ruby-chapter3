a6621a084166437588bea2d01612f867cf2f4905
Mauro Meden <mauro.meden@shopify.com>
2021-07-10 11:50:35 -0600

    Add some creatures methods, add spaceship method

--- space.rb
+++ space.rb
@@ -1,7 +1,5 @@
 class Organism
     attr_accessor :name
-    attr_accessor :total_health
-    attr_accessor :current_health
     
     def print_info
         puts "Organism name: #{name}"
@@ -34,14 +32,10 @@ class Organism
     def sleep
         if name
             puts "#{name.capitalize} falls asleep."
-            puts " --- Sleeping for 8 hours gives #{name.capitalize} 4 health points back."
-            # current_health += 4
-            puts " --- Current health: #{current_health}"
+            puts " --- Sleeping for 8 hours fully restores #{name.capitalize}'s health."
         else
             puts "The organism falls asleep."
-            puts " --- Sleeping for 8 hours gives the creature 4 health points back."
-            # current_health += 4
-            puts " --- Current health: #{current_health}"
+            puts " --- Sleeping for 8 hours fully restores the creature's health."
         end
     end
 end
@@ -49,7 +43,7 @@ end
 class Tube < Organism
 
     def roll
-        puts "Rolling, rolling, rolling."
+        puts "Rolling over objects, the creature finds enough food for today."
     end
 end
 
@@ -57,19 +51,48 @@ class Square < Organism
     def stay_still
         puts "[...] --- nothing seems to happen."
     end
+
+    def absord_light
+        puts "No need to eat for squares. The creature is satisfied"
+    end
 end
 
 class Pyramid < Organism
+    def absord_energy
+        puts "No need to eat for pyramids. The creature is satisfied"
+    end
 end
 
 class Hole < Organism
+    def swallow
+        puts "The creature swallows everything around it, feeling full. Will it last?"
+    end
 end
 
 class Goo < Organism
+    def leave_trail
+        puts "Eugh"
+    end
 end
 
 class Spaceship
+    seats = 5
 
+    def blast_off
+
+        gas_total = 1000
+        gas_consumption = 100
+        gas_used = 0
+
+        puts "Taking off!"
+
+        while gas_used < gas_total
+            puts "Gas remaining: #{gas_total - gas_used}"
+            gas_used += gas_consumption
+        end
+
+        puts "Reached destination!"
+    end
 end
 
 tube = Tube.new
@@ -83,8 +106,10 @@ square.stay_still
 tube.stand_up
 square.stand_up
 
-tube.current_health = 50
 tube.sleep
-
-square.current_health = 10
 square.sleep
+
+Pyramid.new.absord_energy
+Goo.new.leave_trail
+
+Spaceship.new.blast_off
